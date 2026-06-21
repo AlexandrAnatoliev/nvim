79c2deb16bd3a77a9b422aa68e293da9502eb417
Alexandr Anatoliev <per-1986@list.ru>
2026-06-21 16:51:25 +0000

    fix: refactor lsp.lua

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,37 +1,38 @@
 -- ~/.config/nvim/lua/config/lsp.lua
 
 local function setup_lsp_attach()
-    vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(args)
-            local bufnr = args.buf
-            local opts = { buffer = bufnr, noremap = true, silent = true }
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(args)
+      local bufnr = args.buf
+      local opts = {
+        buffer = bufnr,
+        noremap = true,
+        silent = true
+      }
 
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-        end,
-    })
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+    end,
+  })
 end
 
-    -- ============================================================
-    -- Neovim 0.11+ : используем встроенный vim.lsp.config
-    -- ============================================================
-    vim.lsp.config.phpactor = {
-        cmd = { 'phpactor', 'language-server' },
-        filetypes = { 'php' },
-        root_markers = { '.git', 'composer.json' },
-        settings = {},
-        -- внутри vim.lsp.config.phpactor
-        capabilities = require('cmp_nvim_lsp').default_capabilities(),
-    }
-    vim.lsp.enable('phpactor')
-    setup_lsp_attach()
-    print("LSP: vim.lsp.config (phpactor)")
+vim.lsp.config.phpactor = {
+  cmd = { 'phpactor', 'language-server' },
+  filetypes = { 'php' },
+  root_markers = { '.git', 'composer.json' },
+  settings = {},
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+}
+
+vim.lsp.enable('phpactor')
+setup_lsp_attach()
+print("LSP: vim.lsp.config (phpactor)")
 
