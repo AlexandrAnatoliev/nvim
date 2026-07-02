d8077623ec31e09c228cd6da8c78622fc748bed0
Alexandr Anatoliev <per-1986@list.ru>
2026-07-02 08:34:25 +0000

    fix(plugins): add timer plugin

---
+++ lua/plugins/timer/src/Timer.lua
@@ -0,0 +1,18 @@
+local current_dir = debug.getinfo(1, "S").source
+local current_dir = string.match(current_dir, "^@(.+)/[^/]+$")
+
+if not current_dir or current_dir == "" then
+  current_dir = "."
+end
+
+local start_time = os.time()
+
+local start_file_path = current_dir .. "/../data/start_time.txt"
+local file, err = io.open(start_file_path, "w")
+if not file then
+  print("Ошибка при открытии файла" .. err)
+  return
+end
+
+file:write(start_time)
+file:close()
