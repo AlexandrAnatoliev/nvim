20b9d5ca747aaa9cb38fbf16a6f3906dc7158c0c
Alexandr Anatoliev <per-1986@list.ru>
2026-07-02 13:15:00 +0000

    fix(timer): print last session time

--- lua/plugins/timer/src/Timer.lua
+++ lua/plugins/timer/src/Timer.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 function M.start_timer()
-  print("Run timer2\n")
 
   local current_dir = debug.getinfo(1, "S").source
   local current_dir = string.match(current_dir, "^@(.+)/[^/]+$")
@@ -13,16 +12,24 @@ function M.start_timer()
   local start_time = os.time()
 
   local start_file_path = current_dir .. "/../data/start_time.txt"
+  local stop_file_path = current_dir .. "/../data/stop_time.txt"
 
   local file = io.open(start_file_path, "r")
-    if file then
-      local content = file:read("*a")     -- всё содержимое как строка
-      file:close()
-      local number = tonumber(content)
-      if number then
-        print("Число:", number)
-        print("\n")
-    end
+  if file then
+    local content = file:read("*a")     -- всё содержимое как строка
+    file:close()
+    start = tonumber(content)
+  end
+
+  local file = io.open(stop_file_path, "r")
+  if file then
+    local content = file:read("*a")     -- всё содержимое как строка
+    file:close()
+    stop = tonumber(content)
+  end
+
+  if start and stop then
+    print("past session:", stop - start, "sec")
   end
 
   local file, err = io.open(start_file_path, "w")
