2d82f3c5814a8e082ad42f1300afee3c7dda762d
Alexandr Anatoliev <per-1986@list.ru>
2026-08-20 13:36:01 +0000

    fix(mapping): update autotag mapping

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -18,8 +18,15 @@ vim.keymap.set('i', '"', '""<Left>')
 vim.keymap.set('i', "'", "''<Left>")
 
 -- Автоматическое закрытие тегов
-vim.keymap.set('i', '>', function() tag_close() end,
-              { silent = true }) 
+-- vim.keymap.set('i', '>', function() tag_close() end,
+--              { silent = true }) 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "html",
+  callback = function()
+    vim.keymap.set('i', '>', function() tag_close() end,
+             { silent = true }) 
+  end
+})
 
 -- Замена двойных $$ на $
 vim.keymap.set('n', '<leader>ds',
