8bbc0a7dfc2ad43877276270343bcf826cc66c3e
Alexandr Anatoliev <per-1986@list.ru>
2026-09-10 16:00:28 +0000

    fix(mappings): delete useless mapping

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -17,7 +17,6 @@ vim.keymap.set('i', '{', '{}<Left>')
 vim.keymap.set('i', '"', '""<Left>')
 vim.keymap.set('i', "'", "''<Left>")
 vim.keymap.set('i', "`", "``<Left>")
-vim.keymap.set('i', "<", "<><Left>")
 
 -- Автоматическое закрытие тегов
 vim.api.nvim_create_autocmd("FileType", {
